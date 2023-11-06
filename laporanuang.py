23a0c8befad59acf14c9fe2921d370210bbadb6b
amanqs <116822442+amanqs@users.noreply.github.com>
2023-11-06 18:47:37 +0700

    Update laporanuang.py

--- laporanuang.py
+++ laporanuang.py
@@ -22,48 +22,48 @@ logging.basicConfig(level=logging.INFO)
 
 @dp.message_handler(commands=['help','start'])
 async def cara_pengunaan(pesan: types.Message):
-	await pesan.answer('''
-	
-	Selamat Datang Di Bot Laporan Pengeluaran Uang ^_^
-	
-	Contoh Pengunaan :
-		
-		/new kategori #harga item
-		/new makanan #5.000 roti,minuman
-		
-	Perhatian :
-		
-		• Jangan menggunakan spasi dalam kategori,item maupun harga
-		
-		Contoh tidak pake spasi :
-			
-			/new makanan #5.000 roti,makanan
-			
-		Contoh menggunakan spasi :
-			
-			/new makanan #5.000 roti makanan
-			
-	Bisa menggunakan , - _ dan sebagaianya.
-	
-	''')
+    await pesan.answer('''
+    
+    Selamat Datang Di Bot Laporan Pengeluaran Uang ^_^
+    
+    Contoh Pengunaan :
+        
+        /new kategori #harga item
+        /new makanan #5.000 roti,minuman
+        
+    Perhatian :
+        
+        • Jangan menggunakan spasi dalam kategori,item maupun harga
+        
+        Contoh tidak pake spasi :
+            
+            /new makanan #5.000 roti,makanan
+        
+        Contoh menggunakan spasi :
+            
+            /new makanan #5.000 roti makanan
+            
+    Bisa menggunakan , - _ dan sebagaianya.
+    
+    ''')
 
 @dp.message_handler(commands=['new'])
 async def laporan_uang(pesan: types.Message):
-	x = pesan.text.replace('/new','')
-	name = pesan.from_user.first_name
-	id_name = pesan.from_user.id
-	xy = x.split('#')
-	dt = datetime.now()
-	tgln = dt.strftime("%Y-%m-%d/%H:%M:%S")
-	inserts = open_sheets.sheet1
-	try:
-		all = str(tgln)+xy[0]+xy[1]+' '+name+' '+str(id_name)
-		splits = all.split()
-		ins = inserts.append_row(splits)
-		await pesan.answer('Laporan Berhasil !')
-	except:
-		await pesan.answer('Laporan Gagal, Pastikan Cara Pengunaanya Benar !')
-	
+    x = pesan.text.replace('/new','')
+    name = pesan.from_user.first_name
+    id_name = pesan.from_user.id
+    xy = x.split('#')
+    dt = datetime.now()
+    tgln = dt.strftime("%Y-%m-%d/%H:%M:%S")
+    inserts = open_sheets.sheet1
+    try:
+        all = str(tgln)+xy[0]+xy[1]+' '+name+' '+str(id_name)
+        splits = all.split()
+        ins = inserts.append_row(splits)
+        await pesan.answer('Laporan Berhasil !')
+    except:
+        await pesan.answer('Laporan Gagal, Pastikan Cara Pengunaanya Benar !')
+
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     try:
